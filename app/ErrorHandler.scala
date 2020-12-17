698dc2189e5d387f47cdf872bb57b381fb57658f
Maftunbek <maftunbek.r@gmail.com>
2020-12-17 08:55:38 +0500

    uncomment ErrorHandler

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -1,26 +1,26 @@
-//import javax.inject.{Inject, Singleton}
-//import org.webjars.play.WebJarsUtil
-//import play.api.http.HttpErrorHandler
-//import play.api.mvc.Results._
-//import play.api.mvc._
-//import views.html.errors.notFoundPage
-//
-//import scala.concurrent._
-//
-//@Singleton
-//class ErrorHandler  @Inject()(implicit val webJarsUtil: WebJarsUtil,
-//                              notFound: notFoundPage
-//                             ) extends HttpErrorHandler {
-//
-//  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
-//    Future.successful{
-//      Ok(notFound())
-//    }
-//  }
-//
-//  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
-//    Future.successful(
-//      InternalServerError("")
-//    )
-//  }
-//}
+import javax.inject.{Inject, Singleton}
+import org.webjars.play.WebJarsUtil
+import play.api.http.HttpErrorHandler
+import play.api.mvc.Results._
+import play.api.mvc._
+import views.html.notFound
+
+import scala.concurrent._
+
+@Singleton
+class ErrorHandler  @Inject()(implicit val webJarsUtil: WebJarsUtil,
+                              notFound: notFound
+                             ) extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    Future.successful{
+      Ok(notFound())
+    }
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    Future.successful(
+      InternalServerError("")
+    )
+  }
+}
