c325a7f8c2da535a38898912aaf8ed978601c55b
Yunusbek1710 <y.yaqubov.1999@gmail.com>
2021-01-18 20:09:08 +0500

    upload file from coffee

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -177,31 +177,31 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
             } yield {
               val statsAction = StatsAction(LocalDateTime.now, request.host, action = "doc_upload", request.headers.get("Remote-Address").get, request.headers.get("User-Agent").get)
               statsManager ! AddStatsAction(statsAction)
-              Redirect("/doc").flashing("success" -> "File is uploaded")
+              "File is uploaded"
             }).recover {
               case error: Any =>
                 logger.error("Error while uploading image", error)
-                Redirect("/doc").flashing("error" -> "Something went wrong")
+                "Something went wrong"
             }.value
           case None =>
             logger.error("Customer ID not found")
-            Future.successful(Left(Redirect("/doc").flashing("error" -> "Customer ID not found")))
+            Future.successful(Left("Customer ID not found"))
         }
       }.getOrElse {
       logger.debug(s"No file to upload")
-      Future.successful(Left(Redirect(routes.HomeController.index()).flashing("error" -> "Missing file")))
+      Future.successful(Left("Missing file"))
     }
     result.map {
       case Right(redirectWithSuccess) =>
         logger.debug("File successfully uploaded")
-        redirectWithSuccess
+        Ok(redirectWithSuccess)
       case Left(error) =>
         logger.error(s"Something bad happened", error)
-        Redirect("/doc").flashing("error" -> "Something bad happened")
+       BadRequest(error)
     }.recover {
-      case e =>
+      case e: Throwable =>
         logger.error(s"Unexpected error happened", e)
-        Redirect("/doc").flashing("error" -> "Unexpected error happened")
+        BadRequest("Unexpected error happened")
     }
   }
 
